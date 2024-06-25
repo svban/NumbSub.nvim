fa6e27c9a709388058a853a7913c5fb2a90c362c
svban <shb1096@gmail.com>
2024-06-25 15:53:42 +0530

    .

--- lua/numbsub/init.lua
+++ lua/numbsub/init.lua
@@ -1,8 +1,13 @@
 -- Load commands
 local commands = require("numbsub.commands")
 local myCommand = "NumbSub"
+local M = {}
 
 -- Define the custom command in Neovim
-vim.api.nvim_create_user_command(myCommand, function(opts)
-	commands.subst_with_num(opts.args)
-end, { nargs = "*" })
+function M.setup()
+	vim.api.nvim_create_user_command(myCommand, function(opts)
+		commands.subst_with_num(opts.args)
+	end, { nargs = "*" })
+end
+
+return M
